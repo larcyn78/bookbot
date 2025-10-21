83ba8a398dc9404eea619355b644b406cec63858
Larcyn78 <jrasch78@gmail.com>
2025-10-21 14:40:16 -0600

    bookbot completed

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("greetings boots")
+import sys
+from stats import (
+    count_words_in_book,
+    get_chars_dict,
+    chars_dict_to_sorted_list
+)
+
+def get_book_text(book):
+    with open(book) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        bookPath = sys.argv[1]
+        text = get_book_text(bookPath)
+        wordCount = count_words_in_book(text)
+        letterCount = get_chars_dict(text)
+        sortedList = chars_dict_to_sorted_list(letterCount)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {bookPath}")
+        print("----------- Word Count ----------")
+        print(f"Found {wordCount} total words")
+        print("--------- Character Count -------")
+        for dict in sortedList:
+            if not dict["char"].isalpha():
+                continue
+            myChar = dict["char"]
+            num = dict["num"]
+            print(f"{myChar}: {num}")
+        
+
+
+
+main()
+    
---
+++ stats.py
@@ -0,0 +1,25 @@
+def sort_on(items):
+    return items["num"]
+
+def count_words_in_book(book):
+    words = book.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+    
+
